b6c7dea2ba815c3ec97816eebf7a9c2aad790cc7
myflowpl <pedro.blaszczak@gmail.com>
2017-12-01 17:43:21 +0100

    chore: ts-node-dev added for auto reload dev server

--- src/mock/mock-ioc.ts
+++ src/mock/mock-ioc.ts
@@ -1,7 +1,7 @@
+import {iocFactory} from '../ioc';
 import {ContainerModule, Container} from 'inversify';
 import {interactors} from '../interactors/index';
 import {ConfigGetInteractorMock} from './interactors/config-get.interactor.mock';
-import {iocFactory} from '../ioc';
 import {appIoc} from '../rest/app-ioc';
 
 export const mockIoc = new ContainerModule(bind => {
